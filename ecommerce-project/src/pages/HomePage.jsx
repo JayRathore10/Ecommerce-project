5b0793e0475ad38ce5ab0d1ee3bb7b42e551af0c
Jay <jay881532@gmail.com>
2025-09-25 22:21:06 +0530

    Use axios to fetch the data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,17 +1,16 @@
+import axios from 'axios';
 import { Header } from '../components/Header';
 import { products } from '../../starting-code/data/products'
 import checkMark from '../assets/images/icons/checkmark.png'
 import homeFavicon from '../assets/images/home-favicon.png';
 import './HomePage.css'
-import { resolvePath } from 'react-router';
+
 
 export function HomePage() {
 
-  fetch('http://localhost:3000/api/products')
+  axios.get('http://localhost:3000/api/products')
     .then((response) => {
-      return response.json()
-    }).then((data) => {
-      console.log(data);
+      console.log(response.data);
     });
 
   return (
