559d20b2496b287a0f5a27bf347536c717255ed3
Jay <jay881532@gmail.com>
2025-09-26 22:20:46 +0530

    Lift cart up and share it between pages

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,4 +1,6 @@
-import {Route , Routes} from 'react-router';
+import axios from 'axios';
+import { Route, Routes } from 'react-router';
+import { useEffect , useState } from 'react';
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import './App.css'
@@ -8,14 +10,24 @@ import { ErrorPage } from './pages/ErrorPage';
 
 
 function App() {
+
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/cart-items')
+      .then((response) => {
+        setCart(response.data);
+      });
+  } , []);
+
   return (
     <>
       <Routes>
-        <Route index element={<HomePage/>} />
-        <Route path="checkout" element={<CheckoutPage />} />
+        <Route index element={<HomePage cart={cart} />} />
+        <Route path="checkout" element={<CheckoutPage cart={cart} />} />
         <Route path='orders' element={<OrdersPage />} />
         <Route path="tracking" element={<TrackingPage />} />
-        <Route path= "*" element = {<ErrorPage />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </>
   )
--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -6,20 +6,13 @@ import homeFavicon from '../assets/images/home-favicon.png';
 import './HomePage.css'
 
 
-export function HomePage() {
-
+export function HomePage({cart}) {
   const [products, setProducts] = useState([]);
-  const [cart ,setCart] = useState([]);
 
   useEffect(() => {
     axios.get('/api/products')
       .then((response) => {
         setProducts(response.data);
-      });
-
-    axios.get('/api/cart-items')
-      .then((response) => {
-        setCart(response.data);
       });
 
   }, []);
