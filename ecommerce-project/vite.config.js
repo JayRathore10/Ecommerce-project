a22544c25d90393c5634b4c041cc024cd7f62561
Jay <jay881532@gmail.com>
2025-10-01 21:42:27 +0530

    Intsall React Complier

--- ecommerce-project/vite.config.js
+++ ecommerce-project/vite.config.js
@@ -3,13 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server:{
-    proxy:{
-      '/api':{
-        target : 'http://localhost:3000'
-      }, 
-      '/images':{
+  plugins: [react({
+    babel: {
+      plugins: [['babel-plugin-react-compiler', { target: '19' }]],
+    },
+  })],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000'
+      },
+      '/images': {
         target: 'http://localhost:3000'
       }
     }
